ba684db8101a17b6d3aab4b7236891c7f99b5258
Gener Ocena <68327351+geanzie@users.noreply.github.com>
2026-04-11 00:42:06 +0800

    updated nxt config

--- next.config.ts
+++ next.config.ts
@@ -87,7 +87,7 @@ const nextConfig: NextConfig = {
             value: process.env.NODE_ENV === 'development' 
               ? [
                   "default-src 'self'",
-                  "script-src 'self' 'unsafe-inline' https://maps.googleapis.com https://maps.gstatic.com",
+                  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com https://maps.gstatic.com",
                   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
                   "img-src 'self' data: https: https://maps.gstatic.com https://maps.googleapis.com blob:",
                   "font-src 'self' data: https://fonts.gstatic.com",
