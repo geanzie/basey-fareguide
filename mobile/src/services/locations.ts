9d58cb76e077abd3d652468d8e68ce0bb0db937b
Gener Ocena <68327351+geanzie@users.noreply.github.com>
2026-06-22 21:54:09 +0800

    chore: remove stale mobile/ subdir (moved to standalone repo at project root)

--- mobile/src/services/locations.ts
+++
@@ -1,7 +0,0 @@
-import { api } from './api';
-import type { Location } from '@/types/common';
-
-export async function fetchActiveLocations(): Promise<Location[]> {
-  const res = await api.get<{ items: Location[] }>('/api/locations?isActive=true');
-  return res.items;
-}
